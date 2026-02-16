b7ed081b12572932eb7f3625d3b417a8d39e5d03
Harry Thomas <hsthomas@verisk.com>
2026-02-16 14:59:48 -0700

    add PostHog ingestion routes to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,18 @@ const nextConfig: NextConfig = {
         source: '/site.webmanifest',
         destination: '/favicon/site.webmanifest',
       },
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
+      },
     ]
   },
 }
